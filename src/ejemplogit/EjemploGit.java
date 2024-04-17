d3785efb4044442c1ca19bbc12cb946b3a371548
Antonio <blabla@emeai.com>
2024-04-17 11:04:59 +0200

    Version 3.0

--- src/ejemplogit/EjemploGit.java
+++ src/ejemplogit/EjemploGit.java
@@ -1,7 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  * Proyecto editado
+ * Hola más cambios.
  */
 package ejemplogit;
 
@@ -15,7 +14,7 @@ public class EjemploGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code fdjkalsfj alogic here
     }
     
 }
